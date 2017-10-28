22f79ed93109a831874ee24461d79d171f361cb3
Stas Makarov <s.makarov@propellerads.net>
2017-10-29 01:40:01 +0300

    added basic html template

---
+++ app/src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h5> login </h5>
+    <form>
+      <p>
+        name
+        <input type="text"/>
+      </p>
+      <p>
+        password
+        <input type="password"/>
+      </p>
+      <p>
+        <input type="button" value="login"/>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        msg: '?'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/components/Register.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <h5>register</h5>
+  <form>
+    <p>
+      name
+      <input type="text"/>
+    </p>
+    <p>
+      email
+      <input type="email"/>
+    </p>
+    <p>
+      password
+      <input type="password"/>
+    </p>
+    <p>
+      <input type="button" value="ok"/>
+    </p>
+  </form>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'Register'
+  }
+</script>
+
+<style scoped>
+
+</style>
